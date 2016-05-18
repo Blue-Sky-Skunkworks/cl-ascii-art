8a23ca2010ed66b47f9321bd06344e6d5e129302
conrad <whalliburton@gmail.com>
2016-05-18 12:55:27 -0600

    package docs

--- package.lisp
+++ package.lisp
@@ -4,4 +4,10 @@
   (:import-from cl-ansi-text with-color black red green yellow blue magenta cyan white)
   (:import-from uiop directory-files subdirectories)
   (:export text)
-  (:documentation ""))
+  (:documentation "
+    _    ____   ____ ___ ___      _         _      __               ____ _
+   / \  / ___| / ___|_ _|_ _|    / \   _ __| |_   / _| ___  _ __   / ___| |
+  / _ \ \___ \| |    | | | |    / _ \ | '__| __| | |_ / _ \| '__| | |   | |
+ / ___ \ ___) | |___ | | | |   / ___ \| |  | |_  |  _| (_) | |    | |___| |___
+/_/   \_\____/ \____|___|___| /_/   \_\_|   \__| |_|  \___/|_|     \____|_____|
+"))
