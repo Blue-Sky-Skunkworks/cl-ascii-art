dd6e5544366a0f3b642b0d4b3ecf85c90d2e17fc
William Halliburton <whalliburton@gmail.com>
2016-05-25 12:32:52 -0600

    use generators for demos

--- demos.lisp
+++ demos.lisp
@@ -1,29 +1,35 @@
 (in-package :cl-ascii-art)
 
 (defun print-demo (&key (stream *standard-output*)
-                     listvar render-fn
+                     generator
                      count max-height min-height max-width min-width)
-  (iter (for el in (symbol-value listvar))
+  (iter
     (for index from 1 to (or count most-positive-fixnum))
-    (let* ((demo (with-output-to-string (*standard-output*)
-                   (funcall render-fn el)))
-           (count (line-count demo))
-           (line-width (line-width demo)))
-      (when (and (or (null max-height) (<= count max-height))
-                 (or (null min-height) (>= count min-height))
-                 (or (null max-width) (<= line-width max-width))
-                 (or (null min-width) (>= line-width min-width)))
-        (format stream "~%~A~%~%~A" (white el) demo)))))
+    (multiple-value-bind (value remaining) (funcall generator)
+      (destructuring-bind (name . demo) value
+        (let ((count (line-count demo))
+              (line-width (line-width demo)))
+          (when (and (or (null max-height) (<= count max-height))
+                     (or (null min-height) (>= count min-height))
+                     (or (null max-width) (<= line-width max-width))
+                     (or (null min-width) (>= line-width min-width)))
+            (format stream "~%~A~%~%~A" (white name) demo))))
+      (while remaining))))
 
 (defmacro define-demo (name args listvar &body body)
   `(defun ,(symb 'demo- name) (&key ,@args (stream *standard-output*)
                                  count max-height min-height max-width min-width)
      (print-demo :stream stream
-                 :listvar ',listvar
-                 :render-fn (lambda (el)
-                              (handler-case
-                                  (progn ,@body)
-                                (error (c) (warn "Demo errored ~S ~S ~A." ',name el c))))
+                 :generator
+                 (make-list-fn-generator
+                  ,listvar
+                  (lambda (el)
+                    (cons
+                     el
+                     (handler-case
+                         (with-output-to-string (*standard-output*)
+                           ,@body)
+                       (error (c) (warn "Demo errored ~S ~S ~A." ',name el c))))))
                  :count count
                  :max-height max-height
                  :min-height min-height
@@ -35,3 +41,4 @@
 
 (define-demo cows ((text "AbBbCc123!@#")) *cows* (cowsay text :design el))
 (define-demo boxes ((text "AbBbCc123!@#")) *boxes* (boxed text :design el))
+
