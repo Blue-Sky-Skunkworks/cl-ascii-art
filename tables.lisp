aeac2fcb23a36f2f4c3a1ceb22f46a8158faf24d
Your Name <you@example.com>
2017-09-07 09:13:25 -0600

    add :headings option to PRINT-TABLE

--- tables.lisp
+++ tables.lisp
@@ -16,27 +16,35 @@
       (setf (cdr el) (make-list (- length x) :initial-element pad-element))
       (return list))))
 
-(defun print-table (rows &key (stream *standard-output*) (gap "  ") (align :left))
-  (let* ((rows (iter (for row in rows) (collect (ensure-list row))))
-         (max-row-length (apply #'max (mapcar #'length rows)))
-         (base-widths (mapcar (lambda (row) (maximize-length row :key 'princ-to-string :length 'length-mono))
-                              (rotate-rows-to-columns rows)))
-         (control-string (concatenate 'string "~~~D" (ecase align (:right "@") (:left "")) "A"))
-         (control-string-last (concatenate 'string "~" (ecase align (:right "@") (:left "")) "A")))
-    (iter
-      (for row in (mapcar (lambda (row) (pad-list row max-row-length "")) rows))
-      (iter (for els on row)
-        (for width in base-widths)
-        (let ((column (car els)))
-          (format stream
-                  (if (cdr els)
-                      (format nil (concatenate 'string control-string gap)
-                              (+ width (control-length (etypecase column
-                                                         (string column)
-                                                         (t (princ-to-string column))))))
-                      control-string-last)
-                  column)))
-      (terpri stream))))
+(defun print-table (rows &key (stream *standard-output*) (gap "  ") (align :left) hilight
+                           headings)
+  (when rows
+    (let* ((rows (append (and headings (list headings)) (iter (for row in rows) (collect (ensure-list row)))))
+           (max-row-length (apply #'max (mapcar #'length rows)))
+           (base-widths (mapcar (lambda (row) (maximize-length row :key 'princ-to-string :length 'length-mono))
+                                (rotate-rows-to-columns rows)))
+           (control-string (concatenate 'string "~~~D" (ecase align (:right "@") (:left "")) "A"))
+           (control-string-last (concatenate 'string "~" (ecase align (:right "@") (:left "")) "A")))
+      (iter
+        (for row in (mapcar (lambda (row) (pad-list row max-row-length "")) rows))
+        (for rowi from 1)
+        (iter (for els on row)
+          (for width in base-widths)
+          (let* ((column (car els))
+                 (row
+                   (format nil
+                           (if (cdr els)
+                               (format nil (concatenate 'string control-string gap)
+                                       (+ width (control-length (etypecase column
+                                                                  (string column)
+                                                                  (t (princ-to-string column))))))
+                               control-string-last)
+                           column)))
+            (if (or (and headings (= rowi 1))
+                    (and hilight (funcall hilight rowi)))
+                (write-string (white row :effect :bright) stream)
+                (write-string row stream))))
+        (terpri stream)))))
 
 (defun print-selection-table (listvar selectvar &key (reader 'identity) (columns 5) (selection-color :cyan))
   (let ((current (symbol-value selectvar)) ncols)
